fcf6109ea8e85dc3dfd131a917f6b1adb70b499c
Matthew <mattdahl3@gmail.com>
2018-06-08 08:14:11 -0400

    bug fixes

--- app/src/main/java/com/savant/savantandroidteam/GettingStartedFragment.java
+++ app/src/main/java/com/savant/savantandroidteam/GettingStartedFragment.java
@@ -6,10 +6,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class GettingStartedFragment extends Fragment {
 
@@ -21,11 +27,14 @@ public class GettingStartedFragment extends Fragment {
     private Button mProblem;
     private Button mMVP;
 
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_getting_started, container, false);
         ((MainActivity) getActivity()).setTitle("Savant Android Team");
+
 
 
         //UI
@@ -84,4 +93,6 @@ public class GettingStartedFragment extends Fragment {
 
         return view;
     }
+
+
 }
